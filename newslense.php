95480ce0615fa333f83a0f8c874e0d268caa02a6
gillmourtunhira <gillmour8@gmail.com>
2021-07-19 12:43:14 +0200

    edit file titles

--- newslense.php
+++ newslense.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * News-Lense Form
+ * News-Lense
  *
  * @package     News-Lense
  * @author      Centric Data
@@ -9,7 +9,7 @@
  *
 */
 /*
-Plugin Name: News-Lense Form
+Plugin Name: News-Lense
 Plugin URI:  https://github.com/Centric-Data/newslense
 Description: This is a custom newsletter plugin, it can be activated using a plugin shortcode. Its using a two column layout, with custom css.
 Author: Centric Data
@@ -18,9 +18,9 @@ Author URI: https://github.com/Centric-Data
 Text Domain: newslense
 */
 /*
-News-Lense Form is free software: you can redistribute it and/or modify it under the terms of GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version.
+News-Lense is free software: you can redistribute it and/or modify it under the terms of GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version.
 
-News-Lense Form is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+News-Lense is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with Contact-Lense Form.
 */
@@ -31,6 +31,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define variable for path to this plugin file.
-define( 'NLF_LOCATION', dirname( __FILE__ ) );
-define( 'NLF_LOCATION_URL' , plugins_url( '', __FILE__ ) );
-define( 'NLF_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'NL_LOCATION', dirname( __FILE__ ) );
+define( 'NL_LOCATION_URL' , plugins_url( '', __FILE__ ) );
+define( 'NL_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
